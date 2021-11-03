ceb602be2944dee190a045d92efcbce109c8f49d
Anna Prymas <s24284@pjwstk.edu.pl>
2021-11-03 18:32:32 +0100

    pierwsza wersja zadania Echo

---
+++ Zadanie wykład echo/Zadanie wykład echo.cpp
@@ -0,0 +1,89 @@
+﻿// Zadanie wykład echo.cpp : 
+//
+
+# include < algorithm >
+# include < iostream >
+# include < iterator >
+# include < string >
+# include < vector >
+
+
+auto main(int argc, char* argv[]) -> int
+{
+	bool drukujznaknowejlinii = true;
+	bool drukujwodwrotnejkolejnosci = false;
+	bool drukujwoddzielnychliniach = false;
+	int liczbaopcji = 0;
+
+	for (auto i = 1; i < 3 && i < argc; ++i)
+	{
+	
+		
+			std::string opcja = argv[i];
+			if (opcja.compare("-n") == 0)
+			{
+				liczbaopcji++;
+				drukujznaknowejlinii = false;
+			}
+			else if (opcja.compare("-r") == 0)
+			{
+				liczbaopcji++;
+				drukujwodwrotnejkolejnosci = true;
+			}
+			else if (opcja.compare("-l") == 0)
+			{
+				liczbaopcji++;
+				drukujwoddzielnychliniach = true;
+			}
+			else
+				break;
+			
+		
+		
+	}
+
+	if (drukujwodwrotnejkolejnosci)
+	{
+		for (auto i = argc - 1; i >= 1 + liczbaopcji; --i)
+		{
+			std::cout << argv[i];
+			if (i > 1 + liczbaopcji)
+			{
+				std::cout << ", ";
+				if (drukujwoddzielnychliniach)
+				{
+					std::cout << "\n";
+				}
+			}
+		}
+
+	}
+	else
+	{
+		for (auto i = 1 + liczbaopcji; i < argc; ++i)
+
+		{
+			std::cout << argv[i];
+			if (argc - 1 > i)
+			{
+				std::cout << ", ";
+				if (drukujwoddzielnychliniach)
+				{
+					std::cout << "\n";
+				}
+			}
+		}
+
+	}	
+
+
+	
+	if (drukujznaknowejlinii)
+	{
+		std::cout << "\n";
+	}
+
+	
+			
+	return 0;
+}
